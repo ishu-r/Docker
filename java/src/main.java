d7722020e10fe17c4ef03d6895b48b7ccc726c35
ishu <ishu660@gmail.com>
2024-08-17 14:49:30 +0000

    java-docker added

---
+++ java/src/main.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+  
+// Class 
+public class main { 
+  
+    // Main driver method 
+    public static void main(String[] args) 
+    { 
+        // Creating object of date class 
+        Date d1 = new Date(); 
+  
+        // Printing the value stored in above object 
+        System.out.println("today date is " + d1); 
+    } 
+}
+
